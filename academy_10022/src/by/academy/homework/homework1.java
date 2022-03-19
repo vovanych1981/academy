3cb338d208b9b5924480caf9d5a789ad8223d062
vovanych1981 <vovanych1981@gmail.com>
2022-03-19 14:53:33 +0300

    homework1

---
+++ academy_10022/src/by/academy/homework/homework1.java
@@ -0,0 +1,23 @@
+package by.academy.homework;
+
+import java.util.Scanner;
+
+public class homework1 {
+
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		System.out.println("¬вести номер: ");
+		int n = in.nextInt();
+		for (int i = 1; i <= 10; i++) {
+			System.out.println(n + " * " + i + " = " + (n * i));
+
+		}
+		
+		
+		
+	
+	
+	
+	}
+
+}
