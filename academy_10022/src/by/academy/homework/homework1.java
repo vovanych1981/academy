15d6e6d75cf912d570784fb858b3a07912649da5
vovanych1981 <vovanych1981@gmail.com>
2022-03-21 21:59:17 +0300

    homework

--- academy_10022/src/by/academy/homework/homework1.java
+++ academy_10022/src/by/academy/homework/homework1.java
@@ -6,18 +6,13 @@ public class homework1 {
 
 	public static void main(String[] args) {
 		Scanner in = new Scanner(System.in);
-		System.out.println("Ввести номер: ");
+		System.out.println("Р’РІРµСЃС‚Рё РЅРѕРјРµСЂ: ");
 		int n = in.nextInt();
 		for (int i = 1; i <= 10; i++) {
 			System.out.println(n + " * " + i + " = " + (n * i));
 
 		}
 		
-		
-		
-	
-	
-	
 	}
 
 }
