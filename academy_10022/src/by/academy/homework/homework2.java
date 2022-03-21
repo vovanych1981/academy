f482390d98af88c607bbfe9460a267fc18e83986
vovanych1981 <vovanych1981@gmail.com>
2022-03-21 22:34:41 +0300

    homework2

---
+++ academy_10022/src/by/academy/homework/homework2.java
@@ -0,0 +1,12 @@
+package by.academy.homework;
+
+public class homework2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("¬вести номер: ");
+		
+		
+	}
+
+}
